290b0efacd82a75b6888c9fdc69a699431f5b505
viatanas <viktor_atanasov@icloud.com>
2022-09-29 14:13:50 +0100

    dasdsaad

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav className="flex items-center justify-end w-full p-6 space-x-4 text-sm text-gray-900 font-inter">
       <Link href="/retrospectives">
-        <a className="hover:underline">Retrospectives</a>
+        <a className="hidden hover:underline">Retrospectives</a>
       </Link>
       <Link href="/projects">
         <a className="hover:underline">Projects</a>
