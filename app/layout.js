9a0cd74c7b406eba565ffa56be1e4e324521b2ec
viatanas <viktor_atanasov@icloud.com>
2023-11-04 12:35:45 +0000

    changed to app router

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Viktor Atanasov",
+  description: "A space to share thoughts and learnings.",
+  openGraph: {
+    url: "https://viktoratanasov.com",
+    title: "Viktor Atanasov",
+    description: "A space to share thoughts and learnings.",
+    type: "website",
+    image: "/img/og.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
