9db48f83738d834e4d4bc69c454a4a742137e7bf
viatanas <viktor_atanasov@icloud.com>
2023-11-07 20:11:03 +0000

    added writing page again

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata = {
-  metadataBase: new URL("http://localhost:3000"),
+  metadataBase: new URL("https://viktoratanasov.com"),
   title: "Viktor Atanasov",
   description: "A space to share thoughts and learnings.",
   openGraph: {
