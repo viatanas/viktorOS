fd2fb0c520f545f4f8ed1dc51beb0801fc36fb6a
viatanas <viktor_atanasov@icloud.com>
2023-11-07 16:32:13 +0000

    og v4

--- app/layout.js
+++ app/layout.js
@@ -9,11 +9,11 @@ export const metadata = {
     title: "Viktor Atanasov",
     description: "A space to share thoughts and learnings.",
     type: "website",
-    image: {
-      url: "/og.png",
-      width: 1200,
-      height: 630,
-    },
+    // image: {
+    //   url: "/og.png",
+    //   width: 1200,
+    //   height: 630,
+    // },
   },
 };
 
