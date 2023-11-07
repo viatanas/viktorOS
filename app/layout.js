bbc122885ddaa0760d03c06a64d29ee9d26af64e
viatanas <viktor_atanasov@icloud.com>
2023-11-07 20:17:06 +0000

    changes again

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata = {
-  metadataBase: new URL("https://viktoratanasov.com"),
+  // metadataBase: new URL("https://viktoratanasov.com"),
   title: "Viktor Atanasov",
   description: "A space to share thoughts and learnings.",
   openGraph: {
