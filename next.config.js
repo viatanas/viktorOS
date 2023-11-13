0800471d04cc1935df8a256d377c52d597b95225
viatanas <viktor_atanasov@icloud.com>
2023-11-13 20:23:05 +0000

    added mdx and gray matter

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
+const withMDX = require("@next/mdx")();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx"],
   experimental: {
     appDir: true,
   },
 };
+
+module.exports = withMDX(nextConfig);
+
+// module.exports = {
+//   reactStrictMode: true,
+//   experimental: {
+//     appDir: true,
+//   },
+// };
